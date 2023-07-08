25f2af866fcae84e780aa9952d61412613436ec6
adityapeen <adityapeen@gmail.com>
2023-07-08 08:10:36 +0700

    Track last login user

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -37,4 +38,11 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    function authenticated(Request $request, $user)
+{
+    $user->update([
+        'last_login' => \Carbon\Carbon::now()->toDateTimeString()
+    ]);
+}
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,8 @@ class User extends Authenticatable
         'satker_id',
         'level_id',
         'phone',
-        'status'
+        'status',
+        'last_login'
     ];
 
     /**
