85506476143f24aac384bc8aeafa4f19ab0b3c2c
adityapeen <adityapeen@gmail.com>
2023-07-10 14:46:01 +0700

    Fix Update Notes

--- app/Http/Controllers/Admin/NoteController.php
+++ app/Http/Controllers/Admin/NoteController.php
@@ -215,7 +215,6 @@ class NoteController extends Controller
             'end_time' => $request->end_time,
             'max_execute' => $request->max_execute,
             'issues' => $request->issues,
-            'link_drive_notulen' => $request->link_drive_notulen,
             'file_notulen' => $nama_file,
             'status' => 'open',
             'updated_by' => auth()->user()->id,
