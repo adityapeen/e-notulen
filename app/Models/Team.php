010892d8f016b5143f8737d6e0482f858da76173
adityapeen <adityapeen@gmail.com>
2023-07-13 16:32:20 +0700

    Add Team Table

---
+++ app/Models/Team.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Vinkla\Hashids\Facades\Hashids;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class Team extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'name',
+        'satker_id',
+        'created_by',
+        'updated_by',
+    ];
+
+    public function satker()
+    {
+        return $this->belongsTo(MSatker::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function updater()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Hash the ids
+     *
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function id(): Attribute
+    {
+        return  Attribute::make(
+            get: fn ($value) => Hashids::encode($value)
+        );
+    }
+}
