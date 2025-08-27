0dbf96a3b4163504d66ccb12b756bf14c6ce2636
adityapeen <adityapeen@gmail.com>
2025-08-27 17:51:10 +0700

    Simplify User Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,117 +18,12 @@ class UserSeeder extends Seeder
     {
         $default_pass = Hash::make("12345");
         User::create([
-            "name" => "Vebriany Purnamasari" ,
-            "email" => "vebriany.purnamasari@esdm.go.id",
+            "name" => "Superadmin" ,
+            "email" => "superadmin@mail.com",
             "password" => $default_pass,
             "satker_id" => 1,
             "level_id" => 2,
             "phone" => NULL
-        ]);
-        User::create([
-            "name" => "Dara Kurnia Sari" ,
-            "email" => "dara.kurnia@esdm.go.id",
-            "password" => $default_pass,
-            "satker_id" => 8,
-            "level_id" => 2,
-            "phone" => NULL
-        ]);
-        User::create([
-            "name" => "Andhika Sinusaroyo" ,
-            "email" => "andhika.sinusaroyo@esdm.go.id",
-            "password" => $default_pass,
-            "satker_id" => 2,
-            "level_id" => 2,
-            "phone" => NULL
-        ]);
-        User::create([
-            "name" => "Natasha Ruth Ulida Sitorus" ,
-            "email" => "natasha.ulida@esdm.go.id",
-            "password" => $default_pass,
-            "satker_id" => 4,
-            "level_id" => 2,
-            "phone" => NULL
-        ]);
-        User::create([
-            "name" => "Meylad Fitriana" ,
-            "email" => "meylad.f@mail.com",
-            "password" => $default_pass,
-            "satker_id" => 1,
-            "level_id" => 2,
-            "phone" => NULL
-        ]);
-        User::create([
-            "name" => "Aditya Pratama N" ,
-            "email" => "aditya.nugraha@esdm.go.id",
-            "password" => $default_pass,
-            "satker_id" => 5,
-            "level_id" => 2,
-            "phone" => NULL
-        ]);
-        User::create([
-            "name" => "Nidya " ,
-            "email" => "sek.sbp@mail.com",
-            "password" => $default_pass,
-            "satker_id" => 1,
-            "level_id" => 5,
-            "phone" => NULL
-        ]);
-        User::create([
-            "name" => "Dhafi" ,
-            "email" => "sek.bpa@mail.com",
-            "password" => $default_pass,
-            "satker_id" => 2,
-            "level_id" => 5,
-            "phone" => NULL
-        ]);
-        User::create([
-            "name" => "Emma " ,
-            "email" => "sek.bpm@mail.com",
-            "password" => $default_pass,
-            "satker_id" => 3,
-            "level_id" => 5,
-            "phone" => NULL
-        ]);
-        User::create([
-            "name" => "Devi " ,
-            "email" => "sek.bpe@mail.com",
-            "password" => $default_pass,
-            "satker_id" => 4,
-            "level_id" => 5,
-            "phone" => NULL
-        ]);
-        User::create([
-            "name" => "Tely H " ,
-            "email" => "sek.bpg@mail.com",
-            "password" => $default_pass,
-            "satker_id" => 5,
-            "level_id" => 5,
-            "phone" => NULL
-        ]);
-        User::create([
-            "name" => "Dewi" ,
-            "email" => "sek.bpp@mail.com",
-            "password" => $default_pass,
-            "satker_id" => 6,
-            "level_id" => 5,
-            "phone" => NULL
-        ]);
-        User::create([
-            "name" => "Ayu Ratna Wulansari" ,
-            "email" => "sek.bpb@mail.com",
-            "password" => $default_pass,
-            "satker_id" => 7,
-            "level_id" => 5,
-            "phone" => NULL
-        ]);
-        User::create([
-            "name" => "Suci Dwi H " ,
-            "email" => "sek.bdt@mail.com",
-            "password" => $default_pass,
-            "satker_id" => 8,
-            "level_id" => 5,
-            "phone" => NULL
-        ]);
-        
+        ]);        
     }
 }
