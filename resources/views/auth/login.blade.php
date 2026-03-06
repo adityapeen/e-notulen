8c8aea340e66b5490bf1a5985d900981a88bc219
adityapeen <adityapeen@gmail.com>
2026-03-06 14:50:40 +0700

    Update logo on login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,12 +8,12 @@
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
-                    <div class="row align-items-center mb-3"><div class="col text-center"><img src="{{ asset('assets/img/logo-esdm.png')}}" class="img-fluid" ></div></div>
+                    <div class="row align-items-center mb-3"><div class="col text-center"><img src="{{ asset('assets/img/logo-esdm-new.png')}}" class="img-fluid col-6" ></div></div>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
